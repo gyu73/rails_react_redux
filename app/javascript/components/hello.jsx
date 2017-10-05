ab0db4a1f162c286b0d95e833894f3391e060d6b
Masahiro Yagyu <gyu7372@gmail.com>
2017-10-05 14:28:01 +0900

    add eslint

---
+++ app/javascript/components/hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = props => (
+  <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+Hello.propTypes = {
+  name: PropTypes.string
+}
